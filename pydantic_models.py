a3609e24075ce436d0f146b28a37ed477999b249
jinmu333 <dreamhunter2333@gmail.com>
2022-02-15 17:02:07 +0800

    feat: Union[int, None] height width

--- pydantic_models.py
+++ pydantic_models.py
@@ -1,11 +1,11 @@
-from typing import Dict
+from typing import Dict, Union
 from pydantic import BaseModel
 
 
 class Blob(BaseModel):
     url: str
-    width: int
-    height: int
+    width: Union[int, None]
+    height: Union[int, None]
 
 
 class Blobs(BaseModel):
